089257d58e1922ad0824bdebc58bc1c918c28f80
Dave <dwillson@users.noreply.github.com>
2022-12-28 22:35:25 -0800

    enable commands

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3833,7 +3833,7 @@
  * Host Prompt Support enables Marlin to use the host for user prompts so
  * filament runout and other processes can be managed from the host side.
  */
-//#define HOST_ACTION_COMMANDS
+#define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
   //#define HOST_PAUSE_M76
   //#define HOST_PROMPT_SUPPORT
